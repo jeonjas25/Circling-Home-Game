66a8ac894ef1c8c491599c1608ccce6038fe7a36
Jason Jeong <jasonj0618@gmail.com>
2025-03-03 23:02:25 -0800

    Idle, move left, move right, animations

--- Circling Home/Assets/Scripts/PlayerController.cs
+++ Circling Home/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PlayerController : MonoBehaviour
 {
     Vector2 moveInput;
     public float walkSpeed;
-    public bool IsMoving { get; private set; }
+    public bool IsMoving { 
+        get 
+        {
+            return isMoving;
+        } 
+        private set 
+        {
+            isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+    public bool IsMovingLeft { 
+        get 
+        {
+            return isMovingLeft;
+        } 
+        private set 
+        {
+            isMovingLeft = value;
+            animator.SetBool("isMovingLeft", value);
+        }
+    }
+    public bool IsMovingRight { 
+        get 
+        {
+            return isMovingRight;
+        } 
+        private set 
+        {
+            isMovingRight = value;
+            animator.SetBool("isMovingRight", value);
+        }
+    }
+    private bool isMoving = false;
+    private bool isMovingLeft = false;
+    private bool isMovingRight = false;
     private Rigidbody2D rb;
+    Animator animator;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,5 +71,16 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        if (IsMoving)
+        {
+            IsMovingRight = moveInput.x > 0;
+            IsMovingLeft = moveInput.x < 0;
+        }
+        else
+        {
+            IsMovingLeft = false;
+            IsMovingRight = false;
+        }
     }
 }
