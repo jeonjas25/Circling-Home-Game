a0717d88be9e152c79d6068004c795dca520e28c
Jason Jeong <jasonj0618@gmail.com>
2025-04-23 21:52:32 -0700

    Ladder

---
+++ Circling Home/Assets/Scripts/LadderMovement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class LadderMovement : MonoBehaviour
+{
+    private float vertical;
+    public float speed = 8f;
+    private bool isLadder;
+    private bool isClimbing;
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical");
+
+        if (isLadder && Mathf.Abs(vertical) >= 0f)
+        {
+            isClimbing = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * speed);
+        }
+        else 
+        {
+            rb.gravityScale = 1f;
+        }
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+}
