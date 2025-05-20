9cd2777955586230d6e16297c3a1bc9d6a173273
Jason Jeong <jasonj0618@gmail.com>
2025-05-20 10:23:47 -0700

    addded activation distance to moving platforms

--- Circling Home/Assets/Scripts/MovingPlatform.cs
+++ Circling Home/Assets/Scripts/MovingPlatform.cs
@@ -6,26 +6,40 @@ public class MovingPlatform : MonoBehaviour
     public int startingPoint;
     public Transform[] points;
     private int i; //index of array
+    private bool isMoving = false;
+    public float activationDistance = 10f;
+    private Transform playerTransform;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.position = points[startingPoint].position;
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+        if (!isMoving && distanceToPlayer <= activationDistance)
         {
-            i++;
-            if (i == points.Length)
-            {
-                i = 0;
-            }
+            isMoving = true;
         }
-
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        if (isMoving)
+        {
+            if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+            {
+                i++;
+                if (i == points.Length)
+                {
+                    i = 0;
+                }
+            }
+            transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
